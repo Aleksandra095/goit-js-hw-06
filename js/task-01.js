f49629fdd0ba98f105a81d0d369f745f1060c922
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-04-20 14:53:24 +0300

    1

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+
+    const categoriesList = document.querySelectorAll('.item')
+console.log(`Number of categories:${categoriesList.length}`)
+
+categoriesList.forEach(function (a) {
+        console.log(`Category:${a.firstElementChild.textContent}`);
+        console.log(`Elements:${a.lastElementChild.querySelectorAll('li').length}`);
+    });
