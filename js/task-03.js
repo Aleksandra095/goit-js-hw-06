62b1dd24e78a7dd730fc125118da9fd95816f324
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-04-22 10:13:32 +0300

    3

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,4 @@
+const gallery = document.querySelector('.gallery');
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +13,7 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const imagesHTML = images.map(image => `
+  <li><img src="${image.url}" alt="${image.alt}"></li>`).join('');
+gallery.insertAdjacentHTML('beforeend', imagesHTML);
