5e0ca28aed708bdb89977d12d895292dd74d7f22
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-04-23 00:36:01 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener(`sumbit`, (event) => {
+    event.preventDefault();
+const emailInput = loginForm.elements.email;
+const passwordInput = loginForm.elements.password;
 
+if (emailInput.value === '' || passwordInput.value === '') {
+    alert('All fields are required!');
+} else {
+    const formData = {
+    email: emailInput.value,
+    password: passwordInput.value
+};
+    console.log(formData);
+    loginForm.reset();
+}
+});
