49fe81a3d5b566c59396ad3c170186760198c350
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-04-23 19:25:00 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,30 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const control = document.querySelector("#controls");
+const createBtn = controls.querySelector('[data-create]');
+const destroyBtn = controls.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector("#boxes");
+  function getRandomHexColor() {
+    return `#${ Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  function createBoxes(amount) {
+    let size = 30;
+    const boxes = [];
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement("div");
+        box.style.width = '${size}px';
+        box.style.height = '${size}px';
+        box.style.backgroundColor = getRandomHexColor();
+        boxes.push(box);
+        size += 10;
+    }
+    boxesContainer.append(...boxes);
+  }
+  function destroyBoxes() {
+    boxesContainer.innerHTML = "";
+  }
+  createBtn.addEventListener("click",() => {
+const amount = parselnt(control.querySelector('input').value);
+  createBoxes(amount);
+});
+destroyBtn.addEventListener("click", destroyBoxes);
